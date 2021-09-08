9da8aaf9b28f78b43853cb59a99c5849648af93a
AiJiangnan <jnai@gaojihealth.com>
2021-09-08 17:59:24 +0800

    [U] 完成单元测试代码合并

---
+++ spring-test/src/main/java/cn/codeartist/spring/test/TestApplicationContext.java
@@ -0,0 +1,17 @@
+package cn.codeartist.spring.test;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring Test
+ *
+ * @author 艾江南
+ * @date 2020/7/15
+ */
+@Configuration
+@ComponentScan
+public class TestApplicationContext {
+
+
+}
