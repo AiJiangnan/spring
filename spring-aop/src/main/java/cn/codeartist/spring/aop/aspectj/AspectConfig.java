86b406fb1ea0b595735786b901a656c767a16e76
AiJiangnan <904629998@qq.com>
2021-12-26 21:59:21 +0800

    添加Advisor

--- spring-aop/src/main/java/cn/codeartist/spring/aop/aspectj/AspectConfig.java
+++ spring-aop/src/main/java/cn/codeartist/spring/aop/aspectj/AspectConfig.java
@@ -1,9 +1,22 @@
 package cn.codeartist.spring.aop.aspectj;
 
+import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.aop.aspectj.AspectJExpressionPointcutAdvisor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @EnableAspectJAutoProxy
 public class AspectConfig {
+
+    @Bean
+    public AspectJExpressionPointcutAdvisor aspectJExpressionPointcutAdvisor() {
+        AspectJExpressionPointcutAdvisor advisor = new AspectJExpressionPointcutAdvisor();
+        advisor.setExpression("execution(* cn.codeartist.spring.aop.aspectj.*.*(..))");
+        advisor.setAdvice((MethodBeforeAdvice) (method, args, target) -> {
+            // do something
+        });
+        return advisor;
+    }
 }
